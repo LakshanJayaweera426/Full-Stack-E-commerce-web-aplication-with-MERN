04aa330a665d194e082038f4be4bb5329c365345
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2024-06-05 18:30:12 +0900

    Developed Item.jsx and Popular.jsx

--- front-end/src/Components/Item/Item.jsx
+++ front-end/src/Components/Item/Item.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import './Item.css'
 import { Link } from 'react-router-dom'
 
-export const Item = () => {
+
+export const Item = (props) => {
   return (
     <div className='item'>
         <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.image} alt=''/></Link>
