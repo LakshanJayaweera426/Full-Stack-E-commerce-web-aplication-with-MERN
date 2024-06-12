f7d3e51f3abe6d7f83ea7a1f221b7e99fe808639
Lakshan Jayaweera <127861567+LakshanJayaweera426@users.noreply.github.com>
2024-06-12 14:05:11 +0900

    Added function for create popular sections. Added somefunctions for add and remove item in cart

--- front-end/src/Components/NewCollections/NewCollections.jsx
+++ front-end/src/Components/NewCollections/NewCollections.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import './NewCollections.css'
-import new_collection from '../Assets/new_collections'
 import { Item } from '../Item/Item'
 
 export const NewCollections = () => {
 
-  //const [new_collection,setNew_collection] = useState([]);
-  /*
+  const [new_collection,setNew_collection] = useState([]);
+  
   useEffect(()=>{
     fetch('http://localhost:4000/newcollections')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
   },[])
-  */
+  
 
   return (
     <div className='new-collections'>
--- front-end/src/Context/ShopContext.jsx
+++ front-end/src/Context/ShopContext.jsx
@@ -1,12 +1,11 @@
 import React, { createContext, useEffect } from "react";
-import all_product from '../Components/Assets/all_product'
 import { useState } from "react";
 
 export const ShopContext = createContext(null);
 
 const getDefaultCart = () =>{
     let cart = {};
-    for (let index =0; index<all_product.length+1; index++){
+    for (let index =0; index<300+1; index++){
         cart[index] = 0;
     }
     return cart;
@@ -16,15 +15,15 @@ const getDefaultCart = () =>{
 const ShopContextProvider = (props) => {
     
     const [cartItems,setCartItems] = useState(getDefaultCart());
-    /*
-    const [all_product,setAll_Product] = useState([]);
 
+    const [all_product,setAll_Product] = useState([]);
+    
     useEffect(()=>{
         fetch('http://localhost:4000/allproducts')
         .then((response)=>response.json())
         .then((data)=>setAll_Product(data))
     },[])
-
+        
         if(localStorage.getItem('auth-token')){
             fetch('http://localhost:4000/getcart',{
                 method:'POST',
@@ -37,11 +36,11 @@ const ShopContextProvider = (props) => {
             }).then((response)=>response.json())
             .then((data)=>setCartItems(data));
         }
-    */
+    
     const addToCart = (itemId) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/addcart',{
+            fetch('http://localhost:4000/addtocart',{
                 method:'POST',
                 headers:{
                     Accept:'application/form-data',
